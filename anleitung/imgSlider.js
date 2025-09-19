734154d118c0eaaec57dac599c7da2928ffe2dd4
Fata1Error <162109987+Adrian-Schatzmann@users.noreply.github.com>
2025-09-19 14:53:41 +0200

    Add previousImage Button

--- anleitung/imgSlider.js
+++ anleitung/imgSlider.js
@@ -1,8 +1,24 @@
+//Array mit allen Elenemten der Klasse "slide" innerhalb von Elementen mit der ID "slider" erstellen
 const slides = document.querySelectorAll("#slider .slide");
+//Variable für aktuelle Bildposition
 let current = 0;
 
+//Next Image Button
 document.getElementById("nextBtn").addEventListener("click", () => {
-  slides[current].style.display = "none";
-  current = (current + 1) % slides.length;
-  slides[current].style.display = "block";
+  slides[current].style.display = "none"; //aktuelles Bild ausblenden
+  current = current + 1; //Nächstes Bild im Array
+  if (current >= slides.length) { //Zurück auf den Anfang springen wenn das letzte Bild erreicht wurde
+    current = 0;
+  }
+  slides[current].style.display = "block"; //Neues Bild einblenden
+});
+
+//Previous Image Button
+document.getElementById("lastBtn").addEventListener("click", () => {
+  slides[current].style.display = "none"; //aktuelles Bild ausblenden
+  current = current - 1;
+    if (current < 0) {
+    current = slides.length - 1; //Zum letzen Bild springen beim zurückgehen vom ersten Bild
+  }
+  slides[current].style.display = "block"; //Neues Bild einblenden
 });
